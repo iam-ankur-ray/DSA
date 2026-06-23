02f0d8fa0d7783b758272a9e428e99e49f19ef02
Ankur Ray <emailankurat@gmail.com>
2026-06-23 23:04:52 +0530

    normal billing problem in helper

--- DSA/src/main/java/com/ankur/DSA/util/HelperMethods.java
+++ DSA/src/main/java/com/ankur/DSA/util/HelperMethods.java
@@ -3,6 +3,25 @@ package com.ankur.DSA.util;
 import java.util.Scanner;
 
 public class HelperMethods {
-    Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int units = sc.nextInt();
+        double bill;
 
+        if (units <= 50) {
+            bill = units * 0.5;
+        } else if (units <= 150) {
+            bill = (50 * 0.5) + ((units - 50) * 0.75);
+        } else if (units <= 250) {
+            bill = (50 * 0.5) + (100 * 0.75) + ((units - 150) * 1.2);
+        } else {
+            bill = (50 * 0.5) + (100 * 0.75) + (100 * 1.2) + ((units - 250) * 1.5);
+        }
+
+        // Add 20% surcharge
+        bill = bill * 1.2;
+
+        // Print rounded bill
+        System.out.println((int)bill);
+    }
 }
