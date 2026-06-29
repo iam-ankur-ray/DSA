e366f113a8782936e4b14e49874eafb84ed30255
Ankur Ray <emailankurat@gmail.com>
2026-06-29 23:02:03 +0530

    complicated stars

--- DSA/src/main/java/com/ankur/javaQuestions/Patterns.java
+++ DSA/src/main/java/com/ankur/javaQuestions/Patterns.java
@@ -4,7 +4,7 @@ public class Patterns {
     public static void main(String[] args) {
 //        Scanner sc = new Scanner(System.in);
 //        int singleLine = sc.nextInt();
-        printSpaceAndStar(6);
+        printStarsReverse(4);
 
         //Print in single line
 //        for(int i = 0; i < singleLine; i++){
@@ -65,11 +65,74 @@ public class Patterns {
     private static void printSpaceAndStar(int n){
         for(int i = 1; i <= n; i++) {
             System.out.print("*");
+
             int space = n - 2;  // Space should increase by n - 2
             for(int j =1; j <= space; j++){
                 System.out.print(" ");
             }
+
             System.out.println("*");
         }
     }
+
+    private static void actualPyramid(int n){
+        int stars = 1;
+        for(int i = 1; i <=n; i++){
+            int space = n -i;
+            for(int j = i; j<=space;j++){
+                System.out.print(" ");
+            }
+
+            for(int k = 1; k <= stars; k++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+            stars += 2;
+        }
+    }
+
+    // stars = row * 2
+    // space = row + 2 from second row onwards
+    private static void printStars(int n){
+        int space = 0;
+        for(int i = n; i > 0; i--){
+            int stars = i;
+            for(int k = 0; k <= stars; k++){
+                System.out.print("*");
+            }
+
+            for(int j = 0; j < space; j++){
+                System.out.print(" ");
+            }
+
+            for(int k = 0; k <= stars; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+            space +=2;
+        }
+    }
+
+    private static void printStarsReverse(int n){
+        int space = n * 2 -2;
+        for(int i = 0; i < n; i++){
+            int stars = i;
+            for(int k = 0; k <= stars; k++){
+                System.out.print("*");
+            }
+
+            for(int j = 0; j < space; j++){
+                System.out.print(" ");
+            }
+
+            for(int k = 0; k <= stars; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+            space -=2;
+        }
+    }
+
+
 }
