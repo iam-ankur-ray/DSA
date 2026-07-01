76e598d6e908e3c2e0c63d3ee9518d730f011b51
Ankur Ray <emailankurat@gmail.com>
2026-07-01 22:43:46 +0530

    functions

---
+++ DSA/src/main/java/com/ankur/javaQuestions/LearningMethodCreation.java
@@ -0,0 +1,68 @@
+package com.ankur.javaQuestions;
+
+public class LearningMethodCreation {
+
+    public static boolean isEven(int n){
+        return n % 2 == 0;
+    }
+
+    public static String getSize(int n){
+        if(n < 10){
+            return "small";
+        }
+        if(n <= 20){
+            return "medium";
+        }
+        return "large";
+    }
+
+    public static double area(double a, double b){
+        return a * b;
+    }
+
+    public static double areaOfCircle(double r){
+        return Math.PI * r * r;
+    }
+
+    public static void getPrimeNumbers(int n){
+            for(int j = 2; j < n; j++){
+                int count =0;
+               for(int i = 1; i <= j; i++){
+                   if(j%i==0){
+                       count++;
+                   }
+               }
+                if(count==2){
+                    System.out.println(j + " ");
+                }
+            }
+    }
+
+    public static void printPrimes(int n){
+        for(int i = 1; i <=n; i++){
+            if(isPrime(i)) {
+                System.out.println(i + " ");
+            }
+        }
+    }
+
+    public static boolean isPrime(int n){
+        return countFactors(n) == 2;
+    }
+
+    public static int countFactors(int n){
+        int count = 0;
+        for(int i = 1; i <=n; i++){
+            if(n % i == 0){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        printPrimes(100);
+    }
+
+
+}
