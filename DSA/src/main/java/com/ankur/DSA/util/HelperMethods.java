e4a9cba06945599dfdfc0cd1ff58762b2821ee36
Ankur Ray <emailankurat@gmail.com>
2026-07-12 17:33:50 +0530

    factorial in array

--- DSA/src/main/java/com/ankur/DSA/util/HelperMethods.java
+++ DSA/src/main/java/com/ankur/DSA/util/HelperMethods.java
@@ -44,7 +44,7 @@ public class HelperMethods {
 
     public static void printMatrix(int[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
+            for (int j = 0; j < matrix[i].length; j++) {
                 System.out.print(matrix[i][j] + " ");
             }
 
