a278e0204d4281c80e73a02c46a547272b2486a6
Ankur Ray <emailankurat@gmail.com>
2026-08-03 22:03:13 +0530

    static import

--- DSA/src/main/java/com/ankur/DSA/ArrayListQuestions/CheckValidString.java
+++ DSA/src/main/java/com/ankur/DSA/ArrayListQuestions/CheckValidString.java
@@ -1,5 +1,8 @@
 package com.ankur.DSA.ArrayListQuestions;
 
+import static java.lang.Character.isUpperCase;
+import static java.lang.Character.isLowerCase;
+
 public class CheckValidString {
     public static void main(String[] args) {
         System.out.println(isValidString("Goooogle"));
@@ -13,22 +16,22 @@ public class CheckValidString {
         char firstChar = str.charAt(0);
         char secondChar = str.charAt(1);
 
-        boolean expectedUpper = Character.isUpperCase(firstChar) && Character.isUpperCase(secondChar);
-        boolean expectedLower = Character.isUpperCase(firstChar) && Character.isLowerCase(secondChar);
-        boolean firstLower = Character.isLowerCase(firstChar);
+        boolean expectedUpper = isUpperCase(firstChar) && isUpperCase(secondChar);
+        boolean expectedLower = isUpperCase(firstChar) && isLowerCase(secondChar);
+        boolean firstLower = isLowerCase(firstChar);
 
         for(int i = 1; i < str.length(); i++){
             char current = str.charAt(i);
 
-            if(firstLower && Character.isUpperCase(current)){
+            if(firstLower && isUpperCase(current)){
                 return false;
             }
 
-            if(expectedUpper && Character.isLowerCase(current)){
+            if(expectedUpper && isLowerCase(current)){
                 return false;
             }
 
-            if(expectedLower && Character.isUpperCase(current)){
+            if(expectedLower && isUpperCase(current)){
                 return false;
             }
 
