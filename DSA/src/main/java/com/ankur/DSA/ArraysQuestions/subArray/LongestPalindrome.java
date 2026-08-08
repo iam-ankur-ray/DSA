c88ec39ce35f4d872df01f0a74ad76cb6ffa17e6
Ankur Ray <emailankurat@gmail.com>
2026-08-08 11:22:35 +0530

    refinement

--- DSA/src/main/java/com/ankur/DSA/ArraysQuestions/subArray/LongestPalindrome.java
+++ DSA/src/main/java/com/ankur/DSA/ArraysQuestions/subArray/LongestPalindrome.java
@@ -1,33 +1,38 @@
 package com.ankur.DSA.ArraysQuestions.subArray;
 
+import java.util.Scanner;
+
 public class LongestPalindrome {
     public static void main(String[] args) {
-        String a = "bmadamaa";
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any string : ");
+        String a = sc.next();
+        System.out.println();
         System.out.println(longestPalindrome(a));
     }
     public static String longestPalindrome(String A) {
         int size = A.length();
         int start = 0;
-        int end = 0;
+        int end = 1;
         int longest = 1;
 
         //if n is odd
        for(int i = 0; i < size; i++){
            int oddSize = expand(A,i,i);
-           longest = Math.max(longest,oddSize);
-           if(longest == oddSize){
-               start = i - longest/2;
-               end = i + longest/2 + 1;
+           if(longest < oddSize){
+               start = i - oddSize/2;
+               end = i + oddSize/2 + 1;
            }
+           longest = Math.max(longest,oddSize);
        }
-
+        //even
         for(int i = 0; i < size-1; i++){
             int evenSize = expand(A,i,i+1);
-            longest = Math.max(longest,evenSize);
-            if(longest == evenSize){
-                start = i - longest/2;
-                end = i + longest/2 + 1;
+            if(longest < evenSize){
+                start = i - evenSize/2 + 1; //for size 4 and palindrome size 4, mid(i) will be 1, start = 1 - 4/2 -1 = 0, end = 1 + 2 + 1 = 4(condition fulfilled)
+                end = i + evenSize/2 + 1;
             }
+            longest = Math.max(longest,evenSize);
         }
 
        return A.substring(start,end);
