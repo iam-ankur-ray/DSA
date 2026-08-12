c0752ce779d9524354405cda325582a4b345f9df
Ankur Ray <emailankurat@gmail.com>
2026-08-12 20:44:51 +0530

    code cleaning

--- DSA/src/main/java/com/ankur/DSA/StringQuestions/CheckAnagram.java
+++ DSA/src/main/java/com/ankur/DSA/StringQuestions/CheckAnagram.java
@@ -2,29 +2,28 @@ package com.ankur.DSA.StringQuestions;
 
 public class CheckAnagram {
     public static void main(String[] args) {
-        System.out.println(isAnagram("secure","rescue"));
+        System.out.println(isAnagram("secure", "rescue"));
     }
 
     /**
      * Two strings A and B are called anagrams to each other if A can be formed after rearranging the letters of B.
      */
-    public static boolean isAnagram(String A, String B){
-        if(A.length() != B.length()){
+    public static boolean isAnagram(String A, String B) {
+        if (A.length() != B.length()) {
             return false;
-        } else {
-            int[] alphabets = new int[26];
-            for(int i = 0; i < A.length(); i++){
-                alphabets[A.charAt(i) - 'a']++;
-                alphabets[B.charAt(i) - 'a']--;
-            }
-
-            for(int i = 0; i < 26; i++){
-                if(alphabets[i] != 0){
-                    return false;
-                }
-            }
-
-            return true;
         }
+        int[] alphabets = new int[26];
+        for (int i = 0; i < A.length(); i++) {
+            alphabets[A.charAt(i) - 'a']++;
+            alphabets[B.charAt(i) - 'a']--;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (alphabets[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
