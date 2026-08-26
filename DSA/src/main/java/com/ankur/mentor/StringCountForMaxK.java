988f9eb0ba473b19d94863c3207a9b5d31e1dc52
Ankur Ray <emailankurat@gmail.com>
2026-08-26 22:50:55 +0530

    frequency counter final answer

--- DSA/src/main/java/com/ankur/mentor/StringCountForMaxK.java
+++ DSA/src/main/java/com/ankur/mentor/StringCountForMaxK.java
@@ -2,30 +2,39 @@ package com.ankur.mentor;
 
 public class StringCountForMaxK {
     public static void main(String[] args) {
-        String s = "AAABBB";
-        System.out.println(getShortestCount(s,3));
+        String s = "ABABCCCAAAABBCCCBBAA";
+        System.out.println(getShortestCount(s,4));
     }
 
     public static int getShortestCount(String s, int k){
         int answer = 0;
         int i = 0, j = k;
-        while(j < s.length()){
-            int count = 0;
-            for(int ch = i; ch < j; ch++){ 
-                for(int nextCh = i + 1; nextCh < j; nextCh++){
-                    int nextCount = 0;
-                    if(s.charAt(ch) == s.charAt(nextCh)){
-                        nextCount++;
-                    }
-                    count = Math.max(count, nextCount) - 1;
-                }
-            }
-            answer += k - count;
+
+        while(j <= s.length()){
+           int fc = frequencyCount(s.substring(i,j));
+            answer += k - fc;
 
             i += k;
             j += k;
         }
 
         return answer;
+    }
+
+    public static int frequencyCount(String s){
+        int[] frequency = new int[26];
+        s = s.toLowerCase();
+        for(int i = 0; i < s.length(); i++){
+            frequency[s.charAt(i) - 'a']++;
+        }
+
+        int maxFrequency = 0;
+        for(int freq : frequency){
+            if (freq > maxFrequency) {
+                maxFrequency = freq;
+            }
+        }
+
+        return maxFrequency;
     }
 }
